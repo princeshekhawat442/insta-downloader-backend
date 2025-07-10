c47e6844c4c335dea60b16150142e6c83d53cc04
princeshekhawat442 <shekhawatprince442@gmail.com>
2025-07-11 02:07:31 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify
+import instaloader
+
+app = Flask(__name__)
+L = instaloader.Instaloader(download_comments=False, save_metadata=False)
+
+@app.route('/download', methods=['GET'])
+def download_instagram_post():
+    url = request.args.get('url')
+    if not url:
+        return jsonify({"error": "Missing URL"}), 400
+
+    try:
+        shortcode = url.strip('/').split("/")[-1]
+        post = instaloader.Post.from_shortcode(L.context, shortcode)
+
+        media = []
+        if post.typename == "GraphSidecar":
+            for node in post.get_sidecar_nodes():
+                media.append({
+                    "url": node.video_url if node.is_video else node.display_url,
+                    "type": "video" if node.is_video else "photo"
+                })
+        else:
+            media.append({
+                "url": post.video_url if post.is_video else post.url,
+                "type": "video" if post.is_video else "photo"
+            })
+
+        data = {
+            "success": True,
+            "media": media,
+            "caption": post.caption or "",
+            "username": post.owner_username,
+            "likes": post.likes,
+            "comments": post.comments
+        }
+
+        return jsonify(data)
+
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run()
