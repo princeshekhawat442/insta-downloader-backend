6753b56982a882786af5f23769677e4e6ea96671
princeshekhawat442 <shekhawatprince442@gmail.com>
2025-07-11 14:48:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Use the virtual environment Render provides
+# Activate virtual environment
 source /opt/render/project/src/.venv/bin/activate
 
-# Start the app
-gunicorn main:app
-gunicorn bot:app
+# Start both Flask apps in background and foreground
+gunicorn main:app --bind 0.0.0.0:8080 &  # backend
+gunicorn bot:app --bind 0.0.0.0:10000    # telegram bot
